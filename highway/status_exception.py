5b417e188c609ff5fb4398e55c39a75f9a8b3f3d
Farshid A. <farsheed.ashouri@gmail.com>
2025-10-31 22:09:42 +0000

    starting from the ground.

--- highway/status_exception.py
+++
@@ -1,9 +0,0 @@
-from highway.workflow_model import Status
-
-
-class StatusException(Exception):
-    def __init__(self, status: Status, *args) -> None:
-        if status.message:
-            args = args + (status.message,)
-        super().__init__(*args)
-        self.status = status
